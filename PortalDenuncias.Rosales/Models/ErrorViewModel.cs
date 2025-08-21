7e002bd5cffe082b4ccb0db80a39a75b4ee83e97
Felipe Andres Ortiz Alvarez <me@felipealvarez.dev>
2025-08-20 20:51:17 -0400

    - Initial commit

---
+++ PortalDenuncias.Rosales/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortalDenuncias.Rosales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
